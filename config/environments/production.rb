b46ce15a680315b439e43929c5344617ea43c556
Kaitlyn Bradshaw <kaitlynbradshaw@Kaitlyns-MacBook-Pro.local>
2018-05-08 11:45:27 -0500

    got it this time

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,8 +2,14 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
 config.action_mailer.default_url_options = { :host => 'theveldt.herokuapp.com' }
+config.action_mailer.raise_delivery_errors = true
+
+Rails.application.routes.default_url_options[:host] = 'theveldt.herokuapp.com'
 
 config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = false
+config.action_mailer.default :charset => "utf-8"
 # SMTP settings for mailgun
 ActionMailer::Base.smtp_settings = {
   :port           => 587,
