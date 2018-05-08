ebab0665c241e958f353329d4691175caaab77fe
Kaitlyn Bradshaw <kaitlynbradshaw@Kaitlyns-MacBook-Pro.local>
2018-05-08 10:48:56 -0500

    adds mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,14 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
        config.action_mailer.default_url_options = { host: 'theveldt.herokuapp.com/'}
 
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'theveldt.herokuapp.com',
+  :authentication => :plain,
+}
   # Code is not reloaded between requests.
   config.cache_classes = true
 
