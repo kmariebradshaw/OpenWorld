6a324bebcbbbe773ebcda6d771e6e933e1202bc6
Kaitlyn Bradshaw <kaitlynbradshaw@kaitlyns-mbp.attlocal.net>
2018-05-15 19:26:34 -0500

    I got carried away

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
+	  mount_uploader :avatars, AvatarUploader
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
 
-    has_many :portals
-    has_many :comments
+
+  has_many :portals
+  has_many :comments
 end
